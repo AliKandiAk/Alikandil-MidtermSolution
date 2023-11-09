2631af26681bae44f9b15b8bc4c04a90795e92af
AliKandiAk <141944549+AliKandiAk@users.noreply.github.com>
2023-11-10 01:47:36 +0200

    Update midterm_SE.py

--- midterm_SE.py
+++ midterm_SE.py
@@ -1,19 +1,29 @@
 import validators as v #https://snyk.io/blog/secure-python-url-validation/
-tab_list = [{"title": "Youtube", "url": "http://example.com/Youtube"}]
+tab_list = [{"title": "Youtube", "url": "http://example.com/Youtube"},
+             {"title": "Replit", "url": "http://example.com/Replit"}]
 
 def addTab(tab_list,title,url):#choice 1 append a new dictionary to the list
     is_Url=v.url(url)# gives true if its a url or false if not 
     if is_Url:
       new_tab= {"title":title,"url":"https://www."+url+".com"}
       tab_list.append(new_tab)
+      print("Welcome to"+title)
       print(tab_list)
-    else :
+    elif is_Url==False :
         print("incorrect url")
+
 def Close_Tab(tab_list,index=None): # choice 2 delete tab
     if index==None:
         del tab_list[-1] # index pointing top last index in a list and deleting it  
         print("last tab has been closed")
         print(tab_list)
     else: 
-        del tab_list[index] # delit dictionary acording to index
+        del tab_list[index] # delete dictionary acording to index
         print(tab_list)
+
+
+def Switch_Tab(tab_list,index=None):# choice 3 
+    if index is None:
+        print(tab_list[-1])
+    else :
+      print(tab_list[index])
