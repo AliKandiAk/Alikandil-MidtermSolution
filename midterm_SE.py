41b0717d21524689de8c8dd5c7ec016179ddafa2
AliKandiAk <141944549+AliKandiAk@users.noreply.github.com>
2023-11-10 01:02:51 +0200

    Update midterm_SE.py

--- midterm_SE.py
+++ midterm_SE.py
@@ -8,3 +8,12 @@ tab_list = [
 def addTab(tab_list,title,url):#choice 1 append a new dictionary to the list
     new_tab= {"title":title,"url":url}
     tab_list.appemt(new_tab)
+
+def Close_Tab(tab_list,index=None): # choice 2 delete tab
+    if index==None:
+        del tab_list[-1] # index pointing top last index in a list and deleting it  
+        print("last tab has been closed")
+        print(tab_list)
+    else: 
+        del tab_list[index] # delit dictionary acording to index
+        print(tab_list)
