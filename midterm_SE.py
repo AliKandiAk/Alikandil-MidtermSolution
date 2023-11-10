f780a4b79328ebf658cf5c588983636dacdf428b
AliKandiAk <141944549+AliKandiAk@users.noreply.github.com>
2023-11-11 00:46:40 +0200

    Update midterm_SE.py

--- midterm_SE.py
+++ midterm_SE.py
@@ -1,9 +1,32 @@
 import validators as v #https://snyk.io/blog/secure-python-url-validation/
 import requests
 from bs4 import BeautifulSoup 
-tab_list = [{"title": "Youtube", "url": "https://www.youtube.com/"}
-             ]
-
+tab_list = [
+    {
+        "title": "Youtube",
+        "url": "https://www.youtube.com/",
+        "nested_tabs": [
+            {"title": "Subtab 1", "url": "http://example.com/Subtab1"}
+        ]
+    },
+    {
+        "title": "Google",
+        "url": "https://www.google.com/",
+        "nested_tabs": [
+            {"title": "Subtab 2", "url": "http://example.com/Subtab2"},
+            {"title": "Subtab 3", "url": "http://example.com/Subtab3"}
+        ]
+    },
+    {
+        "title": "Facebook",
+        "url": "https://www.facebook.com/",
+        "nested_tabs": [
+            {"title": "Subtab 4", "url": "http://example.com/Subtab4"},
+            {"title": "Subtab 5", "url": "http://example.com/Subtab5"}
+        ]
+    }
+    # Add more tabs as needed
+]
 def addTab(tab_list,title,url):#choice 1 append a new dictionary to the list
     is_Url=v.url(url)# gives true if its a url or false if not 
     if is_Url:
@@ -13,6 +36,7 @@ def addTab(tab_list,title,url):#choice 1 append a new dictionary to the list
       print(tab_list)
     elif is_Url==False :
         print("incorrect url")
+
 
 def Close_Tab(tab_list,index=None): # choice 2 delete tab
     if index==None:
@@ -34,3 +58,29 @@ def Switch_Tab(tab_list,index=None):# choice 3
       html_text=requests.get(tab_list[index]["url"])#https://www.w3schools.com/python/ref_requests_get.asp
       soup=BeautifulSoup(html_text.content,'html.parser')#https://www.educative.io/answers/beautiful-soup-prettify
       print(soup.prettify())  # prittify method got from a wbsite link above 
+
+def Open_nestedTab(tab_list, nested_tab, index):  # choice 5
+    if 0 <= index < len(tab_list): # check for open tabs 
+        if 'nested_tabs' not in tab_list[index]: # if a tab has no nesdted tasb 
+            tab_list[index]['nested_tabs'] = []  # create an empty list for the tab that doesn't have nested tabs
+        tab_list[index]['nested_tabs'].append(nested_tab)  # append the new nested tab
+        print(f"Nested tab added to '{tab_list[index]['title']}' at index {index}.")
+    else:
+        print(f"Invalid index: {index}")
+
+def display_Tabs(tab_list, depth=0):#choice 4 
+    for tab in tab_list:
+        print(" - " * depth + f"{tab['title']}")  # print the title of the main tab
+
+        if 'nested_tabs' in tab:
+            display_Tabs(tab['nested_tabs'], depth + 1)  # recursively display nested tabs with increased depth
+
+
+def clear_Tabs(tablist):#choice 6 
+   tab_list.clear() # cleared all the tabs that are open
+   print("tabs have been cleard")
+
+# Example usage:
+nest = {"title": "Subtab title", "url": "http://example.com/Subtab1"}
+Open_nestedTab(tab_list, nest, 1)
+display_Tabs(tab_list)
