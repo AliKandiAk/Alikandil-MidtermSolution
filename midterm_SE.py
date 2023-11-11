74c89e1822ddc157e72dc126341edf972251236c
AliKandiAk <141944549+AliKandiAk@users.noreply.github.com>
2023-11-11 23:50:51 +0200

    Update midterm_SE.py

--- midterm_SE.py
+++ midterm_SE.py
@@ -22,4 +22,14 @@ def Close_Tab(tab_list,index=None): # choice 2 delete tab
     else: 
         del tab_list[index] # delete dictionary acording to index
         print(tab_list)
+        
+def Switch_Tab(tab_list,index=None):# choice 3 
+    if index is None:
 
+      html_text=requests.get(tab_list[-1]["url"])#https://www.w3schools.com/python/ref_requests_get.asp
+      soup=BeautifulSoup(html_text.content,'html.parser')#https://www.educative.io/answers/beautiful-soup-prettify
+      print(soup.prettify())  # prittify method got from a wbsite link above 
+    else :
+      html_text=requests.get(tab_list[index]["url"])#https://www.w3schools.com/python/ref_requests_get.asp
+      soup=BeautifulSoup(html_text.content,'html.parser')#https://www.educative.io/answers/beautiful-soup-prettify
+      print(soup.prettify())  # prittify method got from a wbsite link above 
