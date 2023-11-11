bfc7b32ace3cccf6990efd4d9a13286e57cb640c
AliKandiAk <141944549+AliKandiAk@users.noreply.github.com>
2023-11-11 23:49:28 +0200

    Update midterm_SE.py

--- midterm_SE.py
+++ midterm_SE.py
@@ -3,3 +3,13 @@ import requests
 import json
 from bs4 import BeautifulSoup
 tab_list=[]
+def addTab(tab_list,title,url):#choice 1 append a new dictionary to the list
+    is_Url=v.url(url)# gives true if its a url or false if not 
+    index=len(tab_list)
+    if is_Url:
+      new_tab= {"title":title,"url":url,"index":index}
+      tab_list.append(new_tab)
+      print("Welcome to"+title)
+      print(tab_list)
+    elif is_Url==False :
+        print("incorrect url")
